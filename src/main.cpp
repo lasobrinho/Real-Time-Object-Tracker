f594ff0bed7015427ccdabce9bcd38338690cfa8
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2016-11-21 20:24:52 -0200

    Fixing subtracted image orientation

--- src/main.cpp
+++ src/main.cpp
@@ -25,20 +25,20 @@ int main( int argc, char** argv )
 	capture = cvCaptureFromFile(argv[1]);
 
 	if (!capture) {
-		fprintf(stderr,"Could not initialize capturing...\n");
-		system("pause");
+		cerr << "Could not initialize capturing" << endl;
+		return 0;
 	}
 
 	frame = cvQueryFrame(capture);
-	frame->origin = 1;
+	frame->origin = 0;
 
-	background = cvCreateImage(cvSize(frame->width, frame->height),frame->depth,1);
-	framecinza = cvCreateImage(cvSize(frame->width, frame->height),frame->depth,1);
-	background->origin = 1;
-	cvCvtColor(frame,background,CV_RGB2GRAY); 
+	background = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
+	framecinza = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
+	background->origin = 0;
+	cvCvtColor(frame, background, CV_RGB2GRAY); 
 
-	foreground = cvCreateImage(cvSize(frame->width, frame->height),frame->depth,1);
-	foreground->origin = 1;
+	foreground = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
+	foreground->origin = 0;
 
 	cvNamedWindow("in", 0);
 	cvNamedWindow("fore", 0);
@@ -49,13 +49,13 @@ int main( int argc, char** argv )
 			break;
 
 		frame = cvRetrieveFrame(capture);
-		cvCvtColor(frame,framecinza,CV_RGB2GRAY);
+		cvCvtColor(frame, framecinza, CV_RGB2GRAY);
 
-		cvAbsDiff(framecinza,background,foreground);	
-		cvThreshold(foreground, foreground,125,255,CV_THRESH_BINARY);
+		cvAbsDiff(framecinza, background, foreground);	
+		cvThreshold(foreground, foreground, 125, 255, CV_THRESH_BINARY);
 
 		cvErode(foreground, foreground, NULL, 1);
-		cvDilate( foreground,foreground,NULL, 2);
+		cvDilate(foreground, foreground, NULL, 2);
 
 		// for(int i=0;i<background->height;i++) 
 		// 	for(int j=0;j<background->width;j++) {
@@ -70,7 +70,7 @@ int main( int argc, char** argv )
 
 	}
 
-	cvReleaseCapture( &capture );
+	cvReleaseCapture(&capture);
 	cvDestroyWindow("in");
 	cvDestroyWindow("fore");
 	system("pause");
