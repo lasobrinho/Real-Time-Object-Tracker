aebdde440eb6a55bf51c498df5203d0b3b5281e4
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2016-11-21 23:02:11 -0200

    Removing noise from input and adjusting cvErode/cvDilate iterations

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main( int argc, char** argv )
 		return 0;
 	}
 
-	IplImage* frame = 0, *invert, *background, *foreground, *framecinza;
+	IplImage* frame = 0, *invert, *background, *foreground, *frameCinza;
 	CvCapture* capture = 0;
 	int pix;
 
@@ -33,37 +33,65 @@ int main( int argc, char** argv )
 	frame->origin = 0;
 
 	background = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
-	framecinza = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
+	frameCinza = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
 	background->origin = 0;
 	cvCvtColor(frame, background, CV_RGB2GRAY); 
+	cvSmooth(background, background, CV_BLUR);
 
 	foreground = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
 	foreground->origin = 0;
 
 	cvNamedWindow("in", 0);
 	cvNamedWindow("fore", 0);
+	cvNamedWindow("frameCinza", 0);
+	cvNamedWindow("background", 0);
 
+	cvMoveWindow("in", 50, 50);
+	cvMoveWindow("fore", 50, 375);
+	cvMoveWindow("frameCinza", 375, 50);
+	cvMoveWindow("background", 375, 375);
+
+	IplConvKernel* structElem;
 
 	for (;;) {
 		if(!cvGrabFrame(capture)) 
 			break;
 
 		frame = cvRetrieveFrame(capture);
-		cvCvtColor(frame, framecinza, CV_RGB2GRAY);
 
-		cvAbsDiff(framecinza, background, foreground);	
-		cvThreshold(foreground, foreground, 125, 255, CV_THRESH_BINARY);
+		cvSmooth(frame, frame, CV_BLUR);
 
-		cvErode(foreground, foreground, NULL, 1);
-		cvDilate(foreground, foreground, NULL, 2);
+		cvCvtColor(frame, frameCinza, CV_RGB2GRAY);		
+
+	    cvAbsDiff(frameCinza, background, foreground);	
+
+	    CvScalar brVal = cvScalarAll(abs(10.0));
+    	cvAddS(foreground, brVal, foreground, NULL);
+	    IplImage *pTempForeground = cvCreateImage(cvGetSize(foreground), IPL_DEPTH_8U, foreground->nChannels);
+	    cvSet( pTempForeground, cvScalarAll(1), NULL );
+	    double scale = 1.5;
+	    cvMul(foreground, pTempForeground, foreground, scale);
+	    cvReleaseImage(&pTempForeground);
+
+		cvThreshold(foreground, foreground, 75, 255, CV_THRESH_BINARY);
+
+		cvErode(foreground, foreground, NULL, 2);
+
+		structElem = cvCreateStructuringElementEx(3, 5, 1, 2, 0);
+		//structElem = cvCreateStructuringElementEx(5, 9, 3, 5, 2);
+
+		cvDilate(foreground, foreground, structElem, 15);
+		cvErode(foreground, foreground, structElem, 10);
 
 		// for(int i=0;i<background->height;i++) 
 		// 	for(int j=0;j<background->width;j++) {
 		// 		((uchar*)(background->imageData + background->widthStep*i))[j] = 255;
 		// 	}  
 
-		cvShowImage("in", frame );
-		cvShowImage("fore", foreground );
+		cvShowImage("frameCinza", frameCinza);
+		cvShowImage("background", background);
+		cvShowImage("in", frame);
+		cvShowImage("fore", foreground);
 
 		if(cvWaitKey(10) >= 0)
 			break;
@@ -71,8 +99,12 @@ int main( int argc, char** argv )
 	}
 
 	cvReleaseCapture(&capture);
+
 	cvDestroyWindow("in");
 	cvDestroyWindow("fore");
+	cvDestroyWindow("frameCinza");
+	cvDestroyWindow("background");
+
 	system("pause");
 
 	return 0;
