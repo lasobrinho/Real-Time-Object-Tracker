b4afa37599e798d8639c8aed4e9d9db1cfe52a77
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2016-11-22 22:49:31 -0200

    Implementing bounding box and center point detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -20,6 +21,10 @@ int main(int argc, char** argv) {
 	Mat frame, frameGray, background, diff;
 	const char *frameWindowTitle = "frame", *frameGrayWindowTitle = "frameGray";
 	const char *backgroundWindowTitle = "background", *diffWindowTitle = "diff";
+	const char *drawingWindowTitle = "drawing";
+	vector<vector<Point> > contours;
+  	vector<Vec4i> hierarchy;
+  	RNG rng(15379);
 
 	// Start VideoCapture using the file name passed as program argument
 	VideoCapture videoCapture(argv[1]);
@@ -39,12 +44,14 @@ int main(int argc, char** argv) {
 	namedWindow(frameGrayWindowTitle, CV_WINDOW_AUTOSIZE);
 	namedWindow(backgroundWindowTitle, CV_WINDOW_AUTOSIZE);
 	namedWindow(diffWindowTitle, CV_WINDOW_AUTOSIZE);
+	namedWindow(drawingWindowTitle, CV_WINDOW_AUTOSIZE);
 
 	// Position output windows on the screen
 	moveWindow(frameWindowTitle, 50, 50);
 	moveWindow(frameGrayWindowTitle, 50, 325);
 	moveWindow(backgroundWindowTitle, 375, 50);
 	moveWindow(diffWindowTitle, 375, 325);
+	moveWindow(drawingWindowTitle, 700, 325);
 
 	// Main program loop
 	for (;;) {
@@ -73,13 +80,29 @@ int main(int argc, char** argv) {
 		dilate(diff, diff, structElementRect, Point(-1, -1), 15);
 		erode(diff, diff, structElementRect, Point(-1, -1), 10);
 
+		// Find contours, get bounding rects and center point of the objects
+		findContours(diff, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+		vector<vector<Point> > contours_poly(contours.size());
+		vector<Rect> boundRect(contours.size());
+		vector<Point2f>center(contours.size());
+		for (int i = 0; i < contours.size(); i++) {
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+			boundRect[i] = boundingRect(Mat(contours_poly[i]));
+		}
 
+		// Draw bounding rects around detected objects
+		Mat drawing = Mat::zeros(diff.size(), CV_8UC3);
+		for (int i = 0; i < contours.size(); i++) {
+			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+			rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+		}
 
 		// Display images
 		imshow(frameWindowTitle, frame);
 		imshow(frameGrayWindowTitle, frameGray);
 		imshow(backgroundWindowTitle, background);
 		imshow(diffWindowTitle, diff);
+		imshow(drawingWindowTitle, drawing);
 
 		// Wait for user input in order to finish the program
 		if (waitKey(10) >= 0) {
