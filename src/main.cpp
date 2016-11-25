84f8bc26b28985650cab73c86ea17ce42c182d41
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2016-11-25 00:19:04 -0200

    Implementing adaptativeBackground() function, improving noise removing funciton

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ void removeNoise(Mat *diff, Background *bg) {
 		}
 	}
 
-	int threshold = 25;
+	int threshold = 10;
 
 	for(int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
@@ -93,6 +93,21 @@ void removeNoise(Mat *diff, Background *bg) {
 	}
 }
 
+void adaptativeBackground(Mat *diff, Mat *bg, vector<vector<int> > *pixelSumGray, int *frameCounter) {
+	int rows = diff->rows;
+	int cols = diff->cols;
+	
+	float avg = 0;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			pixelSumGray->at(i).at(j) = pixelSumGray->at(i).at(j) + diff->at<uchar>(i, j);
+			avg = (float) pixelSumGray->at(i).at(j) / *frameCounter;
+			bg->at<uchar>(i, j) = avg;
+		}
+	}
+}
+
 int main(int argc, char** argv) {
 
 	// Check if the user input is correct to proceed
@@ -114,7 +129,7 @@ int main(int argc, char** argv) {
   	RNG rng(time(NULL));
   	vector<FrameObject> frameObjects;
   	int delayTime;
-  	int frameCounter = 0;
+  	int frameCounter = 1;
 
 	// Start VideoCapture using the file name passed as program argument
 	VideoCapture videoCapture(argv[1]);
@@ -128,11 +143,13 @@ int main(int argc, char** argv) {
 	videoCapture >> frame;
 	cvtColor(frame, background, CV_BGR2GRAY);
 	background.convertTo(background, -1, 1.5, 0);
-	applyBlur(&background, 1);
+	applyBlur(&background, 2);
 
 	Background bg_test(background);
 	vector<vector<int> > ttl_temp(background.rows, vector<int>(background.cols));
 	bg_test.ttl = ttl_temp;
+
+	vector<vector<int> > pixelSumGray(background.rows, vector<int>(background.cols));
 
 	// Initialize windows to display the output
 	namedWindow(frameWindowTitle, CV_WINDOW_AUTOSIZE);
@@ -159,14 +176,15 @@ int main(int argc, char** argv) {
 		// Convert to gray and blur the frame
 		cvtColor(frame, frameGray, CV_BGR2GRAY);
 		frameGray.convertTo(frameGray, -1, 1.5, 0);
-		applyBlur(&frameGray, 1);
+		applyBlur(&frameGray, 2);
+
+		adaptativeBackground(&frameGray, &background, &pixelSumGray, &frameCounter);
 
 	    // Find the difference between the gray frame and the background
 	    absdiff(frameGray, background, diff);
-		threshold(diff, diff, 60, 255, THRESH_BINARY);
+		threshold(diff, diff, 50, 255, THRESH_BINARY);
 
 		removeNoise(&diff, &bg_test);
-		//adaptativeBackground(&diff, &bg_test);
 
 		// Remove noise using erode() function
 		Mat structElementSquare = getStructuringElement(MORPH_RECT, Size(2, 2));
@@ -174,8 +192,8 @@ int main(int argc, char** argv) {
 
 		// Remove gaps inside the objects found
 		Mat structElementRect = getStructuringElement(MORPH_RECT, Size(3, 5));
-		dilate(diff, diff, structElementRect, Point(-1, -1), 10);
-		erode(diff, diff, structElementRect, Point(-1, -1), 7);
+		dilate(diff, diff, structElementRect, Point(-1, -1), 15);
+		erode(diff, diff, structElementRect, Point(-1, -1), 10);
 
 		// Make a copy of the diff Mat, as it will be modified when applying findContours()
 		diff_clone = diff.clone();
@@ -263,11 +281,10 @@ int main(int argc, char** argv) {
 		imshow(frameGrayWindowTitle, frameGray);
 		imshow(backgroundWindowTitle, background);
 		imshow(diffWindowTitle, diff_clone);
-		//imshow(testWindowTitle, test_mat);
 
 		// Set delay between frames based whether the frame has objects or not
 		if (frameObjects.size() > 0) {
-			delayTime = 5;
+			delayTime = 25;
 		} else {
 			delayTime = 2;
 		}
